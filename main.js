5ba595c20b49bf8cae0efbee33d28b82fbfdf3a1
Namlongcomputer <hoandm91@gmail.com>
2019-03-27 16:35:19 +0700

    version2

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 function checkYear() {
     let year = parseInt(document.getElementById("year").value);
-    if (year % 4 == 0){
-        if (year & 100 == 0){
-            if (year % 400 == 0){
-                alert(year + " la nam nhuan");
+    let isLeapYear = false;
+    let isDivisibleBy4 = year % 4 == 0;
+    if (isDivisibleBy4){
+        let isDivisibleBy100 = year & 100 == 0;
+        if (isDivisibleBy100){
+            let isDivisibleBy400 = year % 400 == 0;
+            if (isDivisibleBy400){
+                isLeapYear = true;
             }
             else
-                alert(year + " khong phai la nam nhuan")
+                isLeapYear = false;
         }
-        else
-            alert(year + " la nam nhuan")
     }
+    if (isLeapYear)
+        alert(year + " la nam nhuan")
     else
-        alert(year + " khong phai la nam nhuan")
+        alert(year + " khong phai nam nhuan")
+
 }
